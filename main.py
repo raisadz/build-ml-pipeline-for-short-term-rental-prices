9a64689f477bd8c69f2932040c4aead06ff4b2c9
Raisadz <rdzhamtyrova@protonmail.com>
2022-11-17 18:58:17 +0000

    add data split component

--- main.py
+++ main.py
@@ -77,10 +77,16 @@ def go(config: DictConfig):
             pass
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                version="main",
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"]
+                },
+            )
 
         if "train_random_forest" in active_steps:
 
@@ -105,6 +111,8 @@ def go(config: DictConfig):
             ##################
 
             pass
+
+        
 
 
 if __name__ == "__main__":
